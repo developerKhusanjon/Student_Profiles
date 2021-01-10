6a81ebeca92017f3a7c044db87a307a0adcf1063
Khusanjon Tuychiboyev <developerkhusanjon@gmail.com>
2021-01-10 12:56:39 +0500

    data_source_changed

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,7 @@
 insert into student values (1,'DIF','HUSANJON','DEVELOPER KHUSANJON','2020-12-25');
-insert into student values (2,'DIF','HASANJON','DEVELOPER KHUSANJON','2020-12-25');
-insert into student values (3,'DIF','ILHOMJON','DEVELOPER KHUSANJON','2020-12-25');
-insert into student values (4,'DIF','ZOHIDJON','DEVELOPER KHUSANJON','2020-12-25');
+insert into student values (2,'KIF','HASANJON','DEVELOPER KHASANJON','2020-12-25');
+insert into student values (3,'DIF','ILHOMJON','DEVELOPER ILKHAM','2020-12-25');
+insert into student values (4,'DIF','ZOHIDJON','DEVELOPER ZORRO','2020-12-25');
+insert into student values (5,'DIF','AZIZBEK','DEVELOPER AZIK','2020-12-25');
+insert into student values (6,'KIF','JAMSHIDBEK','DEVELOPER MR','2020-12-25');
+insert into student values (7,'AXF','ILHOMJON','DEVELOPER KHUSANJON','2020-12-25');
